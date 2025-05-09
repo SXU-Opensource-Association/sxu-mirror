6bdd535bebde527cb8e6665e35998a20f3dcfd96
Wenjie zhao <1534779821@qq.com>
2025-05-09 11:14:11 +0800

    33

--- _plugins/addition-exclude.rb
+++
@@ -1,3 +0,0 @@
-Jekyll::Hooks.register(:site, :after_init) do |site|
-  site.config['exclude'].push(*(site.config['addition_exclude'] || []))
-end
--- _src/lib/consts.js
+++
@@ -1,3 +0,0 @@
-export const TUNASYNC_JSON_PATH = "/static/tunasync.json";
-export const ISOINFO_JSON_PATH = "/static/status/isoinfo.json";
-export const DISKINFO_JSON_PATH = "/static/status/disk.json";
